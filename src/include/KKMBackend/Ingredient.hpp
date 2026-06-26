e56effb1d04be61f807d7c64271b65f8a0118251
Mieppe <mapimp9698.yt@gmail.com>
2026-06-26 21:47:41 +0200

    WIP

---
+++ src/include/KKMBackend/Ingredient.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Ingredient
+{
+public:
+    Ingredient() = default;
+    explicit Ingredient(const std::string name) : m_name(std::move(name)){} // std::move to steal resource
+
+    void addIngredient(const std::string& name, int quantity); // should take an unit, maybe a struct, maybe also an ingredient is a class with a given unit associated
+
+    [[nodiscard]] const std::string& getName() const noexcept;
+
+private:
+    std::string m_name{"New Recipe"};
+    std::vector<std::string> m_ingredients{{}}; // should be a vector if ingredient ? or of struc ingredient + quantity ?
+};
